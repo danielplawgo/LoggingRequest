bcdd5ebc2b3330e08558b1e48e48d27cdc3cf6c1
Daniel Plawgo <daniel@plawgo.pl>
2019-01-28 06:05:20 +0100

    dodanie przykładu

---
+++ LoggingRequest/LoggingRequest.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggingRequest.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int instances = 5;
+            var tasks = new Task[instances];
+            for (int i = 0; i < instances; i++)
+            {
+                tasks[i] = Test();
+            }
+
+            Task.WaitAll(tasks);
+        }
+
+        static Task Test()
+        {
+            var webClient = new WebClient();
+
+            return webClient.DownloadStringTaskAsync("http://localhost:55405/");
+        }
+    }
+}
---
+++ LoggingRequest/LoggingRequest/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NLog;
+using Random = System.Random;
+
+namespace LoggingRequest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static ILogger _logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public ActionResult Index()
+        {
+            _logger.Info("Home.Index started.");
+
+            Random random = new Random();
+            var value = random.Next(1, 3);
+            _logger.Info($"Home.Index value is equal {value}");
+
+            if (value == 1)
+            {
+                _logger.Error("Error during processing value");
+                throw new Exception("Error during processing value");
+            }
+
+            _logger.Info("Home.Index ended.");
+            return View();
+        }
+    }
+}
